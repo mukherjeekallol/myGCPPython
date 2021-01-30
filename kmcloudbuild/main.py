7608e4c2713e5cfd5539e8d11ad0f044c68cf193
Kallol Mukherjee <mukherjee.kallol@gmail.com>
2021-01-30 12:01:43 -0500

    trying to create trigger for function.. may be it will work

--- kmcloudbuild/main.py
+++ kmcloudbuild/main.py
@@ -13,4 +13,4 @@ def hellopips(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Hello cloud Dudes!!! Its me again!!!'
+        return f'Hello cloud Dudes!!! Its me again!!!2'
